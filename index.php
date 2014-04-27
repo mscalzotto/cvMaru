fae383835c24c68e0e02329ea57a1d9682608f43
maxiscalzotto <maxiscalzotto@gmail.com>
2014-04-27 16:15:05 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>María Eugenia Tabernero</title>
+		<meta charset="utf-8">
+		<meta keywords="psicologa, psicología, psicologia, conicet, memoria, neuropsicología, neuropsicologia">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script src="js/jquery-latest.js"></script>
+		<script src="js/main.js"></script>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+
+		<link rel="stylesheet" type="text/css" href="css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+	</head>
+	
+	<body>
+		<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+			<div class="container">
+				<div id="mainNav" class="row">
+					<div class="col-xs-12 col-md-8">
+						<p class="logoName">M.Eugenia</p>
+						<p class="logoSurname">Tabernero</p>
+						<p class="logoSpecialty">psicoterapeuta</p>
+					</div>
+					<div class="col-xs-12 col-md-2">
+						<a href="/contacto">icono mail</a>
+					</div>
+					<div class="col-xs-12 col-md-2"></div>
+				</div>
+			</div>
+		</div>
+
+		<div id="menuContainer">
+			<div id="menuButton" class="row">
+				<div class="col-xs-12 col-md-3"></div>
+				<div class="col-xs-12 col-md-2 menuBtn active">
+					<a href="#" class="linkNav">Currículum Vitae</a>
+				</div>
+				<div class="col-xs-12 col-md-2 menuBtn">
+					<a href="#" class="linkNav">Evaluación neuropsicológica</a>
+				</div>
+				<div class="col-xs-12 col-md-2 menuBtn last">
+					<a href="#" class="linkNav">Talleres de memoria y estimulación cognitiva</a>
+				</div>
+				<div class="col-xs-12 col-md-3"></div>
+			</div>
+		</div>
+
+		<div id="mainInformation">
+			<div class="lista">
+				<span class="conicetLogo"></span>
+				<div class="listaTexto">
+					<span class="subTitleBox"><h5>BECARIA DOCTORAL CONICET POR EL TEMA:</h5></span>
+					<p>Investigadora de apoyo en los proyectos UBACYT y CONICET: ‘Simulación motora y Cognición Social, su relación con los sistemas de neuronas en espejo. Estudio en pacientes con Demencia Frontotemporal’, y “Simulación mental en pacientes con Demencia Frontotemporal variante frontal”Investigadora de apoyo en los proyectos UBACYT y CONICET: ‘Simulación motora y Cognición Social, su relación con los sistemas de neuronas en espejo. Estudio en pacientes con Demencia Frontotemporal’, y “Simulación mental en pacientes con Demencia Frontotemporal variante frontal”v</p>
+				</div>
+			</div>
+			<hr>
+			<div class="lista">
+				<span class="conicetLogo"></span>
+				<div class="listaTexto">
+					<span class="subTitleBox"><h5>INVESTIGADORA DE APOYO EN LOS PROYECTOS UBACYT Y CONICET:</h5></span>
+					<p>"Simulación mental en pacientes con Demencia Frontotemporal variante frontal"</p>
+				</div>
+			</div>
+			<hr>
+			<div class="lista">
+				<span class="ubaLogo"></span>
+				<div class="listaTexto">
+					<span class="subTitleBox"><h5>INVESTIGADORA DE APOYO EN LOS PROYECTOS UBACYT Y CONICET:</h5></span>
+					<p>"Simulación motora y Cognición Social, su relación con los sistemas de neuronas en espejo. Estudio en pacientes con Demencia Frontotemporal"</p>
+				</div>
+			</div>
+			<hr>
+		</div>
+
+		<div id="footer" class="container">
+			<p>María Eugenia Tabernero :: Psicoterapeuta</p>
+		</div>
+
+
+	</body>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function($) {
+	$(document).ready(function() {
+		animatedHover();
+	});
+
+	function animatedHover() {
+		$('div#menuButton a.linkNav').mouseover(function() {
+			if (!$(this).parent().hasClass('active')) {
+				$(this).parent().animate({
+					bottom : 0
+				}, 800);
+			}
+		});
+
+		$('div#menuButton a.linkNav').mouseout(function() {
+			if (!$(this).parent().hasClass('active')) {
+				$(this).parent().animate({
+					bottom : "25px"
+				}, 800);
+			}
+		});
+	}
+
+})(jQuery);
